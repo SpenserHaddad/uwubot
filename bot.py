3724e4e2af76a6d19901dea81541d5779020e6f8
Spenser Haddad <spenser.haddad@gmail.com>
2021-04-01 20:22:48 -0400

    Undo print debugs

--- bot.py
+++ bot.py
@@ -52,11 +52,7 @@ async def on_message(message: Message):
         and not message.content.startswith("!unchaos")
     ):
         uwufy_message = uwufy(message.content)
-        print("Deleting old message:")
-        try:
-            await message.delete()
-        except Exception as e:
-            print(f"Failed to delete message: {e}")
+        await message.delete()
         await channel.send(uwufy_message)
     await bot.process_commands(message)
 
