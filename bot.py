d9cf605ba4a4ada2a3eb833f37ccf5c62b1f7197
Spenser Haddad <spenser.haddad@gmail.com>
2021-04-01 21:31:13 -0400

    Move content to field so @mentions work

--- bot.py
+++ bot.py
@@ -54,8 +54,9 @@ async def on_message(message: Message):
     ):
         author_name = message.author.display_name
         uwufy_message = uwufy(message.content)
-        embed = Embed(title=uwufy_message, colour=Colour(0xFF69B4))
+        embed = Embed(colour=Colour(0xFF69B4))
         embed.set_author(name=author_name, icon_url=message.author.avatar_url)
+        embed.add_field(uwufy_message)
         embed.set_footer(text=EMBED_FOOTER)
         await message.delete()
         await channel.send(embed=embed)
